fa9d7bfa60f185f50216b198c25a8d47046f3bc0
LorenzoMegna24 <megnalorenzo0@gmial.com>
2023-04-18 16:15:43 +0200

    Bonus completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Benvenuto Vue!'
+      message: 'Benvenuto Vue!',
+      img: "https://picsum.photos/200"
     }
   },
   methods: {
